e9b052e2841e11800c9b4b54048fe8c5cc04b397
Kamolbek <rusmirolub@gmail.com>
2020-04-10 20:42:56 +0100

    Initial Commit

---
+++ SkyWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SkyWeather
+//
+//  Created by Komolbek Ibragimov on 4/10/20.
+//  Copyright © 2020 VladMus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
